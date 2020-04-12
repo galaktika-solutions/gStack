e6652afb3b0b08ac13dbe75b39638b7eaf9fdfb8
Zoltan Egyed <zoltan.egyed@vertis.com>
2020-04-12 12:37:07 +0200

    added missing abort requests

--- js_client/src/dashboard.jsx
+++ js_client/src/dashboard.jsx
@@ -2,21 +2,35 @@
 var React = require("react");
 
 var Ajax = require("./ajax.jsx");
+var AbortRequests = require("../utils/abort_requests.js");
 
 class Dashboard extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    window.title = gettext("Dashboard");
+    this.requests = [];
+    this.state = {
+      data: []
+    };
+  }
   componentDidMount() {
     Ajax({
       url: "/api/core/key_value_store/",
       statusCode: {
         200: response => {
+          this.setState({data: response});
           console.log(response);
         }
       }
     });
   }
 
+  componentWillUnmount() {
+    AbortRequests(this.requests);
+  }
+
   render() {
-    return <div className="content">{gettext("Welcome on the Dasboard page")}</div>;
+    return <div>{gettext("Welcome on the Dasboard page")}</div>;
   }
 }
 
