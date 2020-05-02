8c214848bd841e4b93ebe00446176d7bee1a1bbb
Zoltan Egyed <zoltan.egyed@vertis.com>
2020-05-02 14:13:49 +0200

    start page is done

--- js_client/src/dashboard.jsx
+++ js_client/src/dashboard.jsx
@@ -969,28 +969,20 @@ class Dashboard extends React.PureComponent {
         {/* <!-- Partner Logo Section Begin --> */}
         <div className="partner-logo">
           <div className="container">
-            <div className="logo-item">
-              <div className="tablecell-inner">
+            <div className="row">
+              <div className="logo-item col-lg-2 offset-lg-1">
                 <img src={`${config.staticUrl}images/logo-carousel/logo-1.png`} alt="" />
               </div>
-            </div>
-            <div className="logo-item">
-              <div className="tablecell-inner">
+              <div className="logo-item col-lg-2">
                 <img src={`${config.staticUrl}images/logo-carousel/logo-2.png`} alt="" />
               </div>
-            </div>
-            <div className="logo-item">
-              <div className="tablecell-inner">
+              <div className="logo-item col-lg-2">
                 <img src={`${config.staticUrl}images/logo-carousel/logo-3.png`} alt="" />
               </div>
-            </div>
-            <div className="logo-item">
-              <div className="tablecell-inner">
+              <div className="logo-item col-lg-2">
                 <img src={`${config.staticUrl}images/logo-carousel/logo-4.png`} alt="" />
               </div>
-            </div>
-            <div className="logo-item">
-              <div className="tablecell-inner">
+              <div className="logo-item col-lg-2">
                 <img src={`${config.staticUrl}images/logo-carousel/logo-5.png`} alt="" />
               </div>
             </div>
@@ -998,11 +990,115 @@ class Dashboard extends React.PureComponent {
         </div>
         {/* <!-- Partner Logo Section End --> */}
 
-        <div style={{height: "4000px"}}>
-          asd
-          <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
-          <br /> <br /> <br /> <br /> <br /> <br />
-        </div>
+        {/* <!-- Footer Section Begin --> */}
+        <footer className="footer-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3">
+                <div className="footer-left">
+                  <div className="footer-logo">
+                    <a href="#">
+                      <img src={`${config.staticUrl}images/footer-logo.png`} alt="" />
+                    </a>
+                  </div>
+                  <ul>
+                    <li>Address: 60-49 Road 11378 New York</li>
+                    <li>Phone: +65 11.188.888</li>
+                    <li>Email: hello.colorlib@gmail.com</li>
+                  </ul>
+                  <div className="footer-social">
+                    <a href="#">
+                      <i className="fa fa-facebook" style={{marginTop: "11px"}} />
+                    </a>
+                    <a href="#">
+                      <i className="fa fa-instagram" style={{marginTop: "11px"}} />
+                    </a>
+                    <a href="#">
+                      <i className="fa fa-twitter" style={{marginTop: "11px"}} />
+                    </a>
+                    <a href="#">
+                      <i className="fa fa-pinterest" style={{marginTop: "11px"}} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-2 offset-lg-1">
+                <div className="footer-widget">
+                  <h5>Information</h5>
+                  <ul>
+                    <li>
+                      <a href="#">About Us</a>
+                    </li>
+                    <li>
+                      <a href="#">Checkout</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact</a>
+                    </li>
+                    <li>
+                      <a href="#">Serivius</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-2">
+                <div className="footer-widget">
+                  <h5>My Account</h5>
+                  <ul>
+                    <li>
+                      <a href="#">My Account</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact</a>
+                    </li>
+                    <li>
+                      <a href="#">Shopping Cart</a>
+                    </li>
+                    <li>
+                      <a href="#">Shop</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="newslatter-item">
+                  <h5>Join Our Newsletter Now</h5>
+                  <p>Get E-mail updates about our latest shop and special offers.</p>
+                  <form action="#" className="subscribe-form">
+                    <input type="text" placeholder="Enter Your Mail" />
+                    <button type="button">Subscribe</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="copyright-reserved">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="copyright-text">
+                    {/*
+                      Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0
+                    */}
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All
+                    rights reserved | This template is made with{" "}
+                    <i className="fa fa-heart-o" aria-hidden="true" /> by{" "}
+                    <a href="https://colorlib.com" target="_blank">
+                      Colorlib
+                    </a>
+                    {/*
+                      Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0.
+                    */}
+                  </div>
+                  <div className="payment-pic">
+                    <img src="img/payment-method.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+        {/* <!-- Footer Section End --> */}
       </BlockUi>
     );
   }
